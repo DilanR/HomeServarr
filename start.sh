46a06e98f1f6a76bc2d5a9eb470a19c44fe3a6a2
dilan <dilanredha@hotmail.com>
2025-04-07 18:00:59 +0200

    Refactor, looks more pleasant.

--- start.sh
+++ start.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-sudo docker network create backend
-sudo docker network create frontend
+if [  -f "./.env" ]; then
+  echo ".env exists, no more configuration required."	
+else
+  echo "check readme for further configuration"
+fi
 
-sudo docker compose -f docker-compose.network.yml up -d
-sudo docker compose -f docker-compose.jellyfin.yml up -d
-sudo docker compose -f docker-compose.jellyfin-backend.yml up -d
+# Create acme.json for Traefik if not present
+if [ ! -f "./traefik/letsencrypt/acme.json" ]; then
+  echo "Creating traefik/letsencrypt/acme.json..."
+  mkdir -p ./traefik/letsencrypt
+  touch ./traefik/letsencrypt/acme.json
+  chmod 600 ./traefik/letsencrypt/acme.json
+fi
+
+sudo docker network create homeservarr_bridge 2&> /dev/null
+
+sudo docker compose -f docker-compose.yml up -d
